577e7055dfc8ec6e8e23242c1a4795e192773574
WELLINGTON DE SOUSA ALMEIDA <wellington.salmeida7@senacsp.edu.br>
2018-04-13 09:34:32 -0300

    Initial commit

---
+++ app/src/main/java/br/com/senacsp/filmesapp/Api.java
@@ -0,0 +1,15 @@
+package br.com.senacsp.filmesapp;
+
+public class Api {
+
+
+    private static final String ROOT_URL = "http://10.23.49.34:8080/HeroApi/v1/Api.php?apicall=";
+
+    public static final String URL_CREATE_HERO = ROOT_URL + "createhero";
+    public static final String URL_READ_HEROES = ROOT_URL + "getheroes";
+    public static final String URL_UPDATE_HERO = ROOT_URL + "updatehero";
+    public static final String URL_DELETE_HERO = ROOT_URL + "deletehero&id=";
+
+
+
+}
---
+++ app/src/main/java/br/com/senacsp/filmesapp/Hero.java
@@ -0,0 +1,58 @@
+package br.com.senacsp.filmesapp;
+
+public class Hero {
+
+    private int id;
+    private String name, realname;
+    private int rating;
+    private String teamaffiliation;
+
+    public Hero(int id, String name, String realname, int rating, String teamaffiliation) {
+        this.id = id;
+        this.name = name;
+        this.realname = realname;
+        this.rating = rating;
+        this.teamaffiliation = teamaffiliation;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getTeamaffiliation() {
+        return teamaffiliation;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setTeamaffiliation(String teamaffiliation) {
+        this.teamaffiliation = teamaffiliation;
+    }
+}
